9ccbd3006e1c685761d65b54f52780e6bf7d285d
TheCoderZeus <davieumrei667@gmail.com>
2025-05-26 17:48:29 -0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DarkCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            Display.Text += button.Content.ToString();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "";
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string expression = Display.Text;
+                var result = new DataTable().Compute(expression, null);
+                Display.Text = result.ToString();
+            }
+            catch
+            {
+                Display.Text = "Erro";
+            }
+        }
+    }
+}
